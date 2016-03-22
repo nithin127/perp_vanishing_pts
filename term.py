462c0087914403706932363e44cfb170b9357401
nithin127 <nithin127vasisth@gmail.com>
2016-03-22 19:06:51 +0530

    Trying to figure out the intersection errors

--- term.py
+++ term.py
@@ -1,32 +1,86 @@
-max_len = find_max_length(line)
-vote =[]
-for point in intersection_valid[2:3]:
-	vote_iter = float(0)
-	if (point[2] == float("inf")):
-		pts = line[point[1]][0]
-		if (pts[0]-pts[2] ==0):
-			m = float("inf")
+zero_slope = []
+inf_slope=[]
+other_line=[]
+
+for i in range(line.shape[0]):
+	if (line[i][0][3]-line[i][0][1] == 0):
+		#print line[i][0] , 'zero_slope'
+		zero_slope.append(line[i])
+		cv2.line(img,(int(line[i][0][0]),int(line[i][0][1])),(int(line[i][0][2]),int(line[i][0][3])),(255,0,0),2)
+
+for i in range(line.shape[0]):
+	if (line[i][0][2]-line[i][0][0] == 0):
+		#print line[i][0] , 'inf_slope'
+		inf_slope.append(line[i])
+		cv2.line(img,(int(line[i][0][0]),int(line[i][0][1])),(int(line[i][0][2]),int(line[i][0][3])),(0,0,255),2)
+
+for i in range(line.shape[0]):
+	if not((line[i][0][2]-line[i][0][0] == 0)or(line[i][0][3]-line[i][0][1] == 0)):
+		other_line.append(line[i])
+		#print line[i][0], mi
+
+cv2.imwrite('houghlines_'+image_dir[-6:],img)
+
+
+# the list would be appended in the following format:
+# line1, line2, x_intersection, y_intersection, (1==outside the line segment, 2== inside the line segment)
+
+intersection = []
+intersection_valid = []
+intersection_invalid = []
+
+for i in range(len(other_line)):
+	mi = (other_line[i][0][3]-other_line[i][0][1])/(other_line[i][0][2]-other_line[i][0][0])
+	for j in range(i+1,len(other_line)):
+		##
+		#checking if any of the slopes are infinity
+		##
+		mj = (other_line[j][0][3]-other_line[j][0][1])/(other_line[j][0][2]-other_line[j][0][0])	
+		if (mi==mj):
+			intersection.append([i,j,float("inf"),float("inf"),1])
+			intersection_valid.append([i,j,float("inf"),float("inf"),1])
 		else:
-			m = (pts[1]-pts[3])/pts[0]-pts[2]
-		for seg in line[9:10]:
-			pts_t = seg[0]			
-			if (pts[0]-pts[2] ==0):
-				m_t = float("inf")
-			else:
-				m_t = (pts[1]-pts[3])/pts[0]-pts[2]
-			length_t = np.sqrt((pts[1]-pts[3])**2+(pts[0]-pts[2])**2)
-			#vote_iter = vote_iter + vote_value(m,m_t,length_t,max_len)
-	else:
-		v_x = point[2]
-		v_y = point[3]
-		for seg in line[9:10]:
-			pts = seg[0]			
-			if (pts[0]-pts[2] ==0):
-				m_t = float("inf")
-			else:
-				m_t = (pts[1]-pts[3])/pts[0]-pts[2]
-			length_t = np.sqrt((pts[1]-pts[3])**2+(pts[0]-pts[2])**2)
-			m = (0.5*(pts[1]+pts[3])-v_y)/(0.5*(pts[0]-pts[2])-v_x)
-			vote_iter = vote_iter + vote_value(m,m_t,length_t,max_len)
-	vote.append(vote_iter)
-return vote
+			p_x = (other_line[j][0][1]-other_line[i][0][1] + mi*other_line[i][0][0] - mj*other_line[j][0][0])/(mi-mj)
+			p_y = other_line[j][0][1]+ mj*(p_x-other_line[j][0][0])
+			if (in_lineseg(other_line[i][0],p_x,p_y) or (in_lineseg(other_line[j][0],p_x,p_y))):
+				intersection.append([i,j,p_x,p_y,0])
+				intersection_invalid.append([i,j,p_x,p_y,0])
+			else: 
+				intersection.append([i,j,p_x,p_y,1])
+				intersection_valid.append([i,j,p_x,p_y,1])
+
+
+image_dir = 'groundtruth/Images/0000000041.jpg'
+img1 = cv2.imread(image_dir)
+for i in range(len(intersection_valid)):
+	if (intersection_valid[i][2]!=float("inf")):
+		cv2.circle(img1,(int(intersection_valid[i][2]),int(intersection_valid[i][3])),10,(100,50,50),-1)
+		ll1 = intersection_valid[i][0]
+		ll2 = intersection_valid[i][1]
+		cv2.line(img1,(int(other_line[ll1][0][0]),int(other_line[ll1][0][1])),(int(other_line[ll1][0][2]),int(other_line[ll1][0][3])),(0,255,0),2)
+		cv2.line(img1,(int(other_line[ll2][0][0]),int(other_line[ll2][0][1])),(int(other_line[ll2][0][2]),int(other_line[ll2][0][3])),(0,0,255),2)
+		cv2.imwrite('intersection_val_'+str(i)+'_'+image_dir[-6:],img1)
+		img1 = cv2.imread(image_dir)
+
+
+con = other_line[3][0]
+pro = other_line[5][0]
+img1 = cv2.imread(image_dir)
+ll1 = 3
+cv2.line(img1,(int(other_line[ll1][0][0]),int(other_line[ll1][0][1])),(int(other_line[ll1][0][2]),int(other_line[ll1][0][3])),(0,255,100),2)
+ll1 = 5
+cv2.line(img1,(int(other_line[ll1][0][0]),int(other_line[ll1][0][1])),(int(other_line[ll1][0][2]),int(other_line[ll1][0][3])),(0,100,255),2)
+
+#lets complete the lines
+i = 3
+m_con = (other_line[i][0][3]-other_line[i][0][1])/(other_line[i][0][2]-other_line[i][0][0])
+i = 5
+m_pro = (other_line[i][0][3]-other_line[i][0][1])/(other_line[i][0][2]-other_line[i][0][0])
+for g in range(500):
+	y_con = other_line[3][0][1]+m_con(g-other_line[3][0][1])
+	y_pro = other_line[5][0][1]+m_con(g-other_line[5][0][1])
+	cv2.circle(img1,(int(intersection_valid[i][2]),int(intersection_valid[i][2])),3,(100,50,50),-1)
+
+
+img = img_hough
+
--- vanishing_points_new.py
+++ vanishing_points_new.py
@@ -6,7 +6,7 @@ from PIL import Image
 from tempfile import TemporaryFile
 import pdb
 
-from functions import find_max_length, find_intersection, rank_vanishing_points, vote_value
+from functions import find_max_length, find_intersection, rank_vanishing_points, vote_value, in_lineseg
 
 ########################################
 #In this section, we detect the edges of the image using Probablistic houghtransform. 
@@ -17,21 +17,17 @@ gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 edges = cv2.Canny(gray,50,170,apertureSize = 3)
 minLineLength = 100
 maxLineGap = 10
-lines = cv2.HoughLinesP(edges,1,np.pi/180,100,minLineLength,maxLineGap)
-t = lines.shape[0]
+line = cv2.HoughLinesP(edges,1,np.pi/180,100,minLineLength,maxLineGap)
+t = line.shape[0]
 for num in xrange(t):
-	x1,y1,x2,y2 = lines[num][0]
+	x1,y1,x2,y2 = line[num][0]
 	cv2.line(img,(x1,y1),(x2,y2),(0,255,0),2)
 
-cv2.imwrite('houghlines5.jpg',img)
+#cv2.imwrite('houghlines_'+image_dir[-6:],img)
 #just to confirm houghlines detection
 
-line = lines.astype(float);
-
-############# The part after this is only for testing #############
-# We deternine the maximum length of the segements that are detected
-
-maxlength, i_arg = find_max_length(line)
+line = line.astype(float);
+img_hough = img
 
 ###### intersection detection ######
 
@@ -41,214 +37,27 @@ intersection, intersection_valid, intersection_invalid = find_intersection(line)
 #	print intersection_valid[i]
 
 ###### voting of the detected intersection points ######
-vote = rank_vanishing_points(intersection_valid, line)
+#vote = rank_vanishing_points(intersection_valid, line)
 
-points_sel = []
-for j in range(len(vote)):
-	i = vote[j]
-	if(not(not(i>3) and not(i<3))):
-		points_sel.append(intersection_valid[j])
-		print i
-
-
-for i in range(len(vote)):
-	print vote[i]
 '''
-N = np.shape(intersection_valid);
-w_1 = 0.10; 
-w_2 = 0.99;
-d_max = 0 ;
-vote_data = np.zeros((N[0],8))
-vote_data_valid = [];
-vote_data_invalid = [];
-for i in range(N[0]):
-	I = intersection_valid[i][0];
-	J = intersection_valid[i][1];
-	x_mid_I =  (line[I,0]+line[I,2])/2;
-	y_mid_I =  (line[I,1]+line[I,3])/2;
-	if ((line[I,3] == line[I,1]) & (line[J,2] == line[J,0])):
-		d_1 = float("inf");
-	elif((line[I,3] == line[I,1])):
-		slope2 = (line[J,3]-line[J,1])/(line[J,2]-line[J,0]);
-		x_int = x_mid_I;
-		y_int = line[J,1] + slope2*(x_int-line[J,0]);
-		d_1 = math.sqrt((x_mid_I-x_int)**2 + (y_mid_I-y_int)**2);
-	elif ((line[J,2] == line[J,0])):
-		slope1 =(line[I,0]-line[I,2])/(line[I,3]-line[I,1]);
-		x_int = line[J,0];
-		y_int = y_mid_I +slope1*(x_int-x_mid_I);
-		d_1 = math.sqrt((x_mid_I-x_int)**2 + (y_mid_I-y_int)**2);
-	else :
-		slope1 =(line[I,0]-line[I,2])/(line[I,3]-line[I,1]);
-		slope2 = (line[J,3]-line[J,1])/(line[J,2]-line[J,0]);
-		if (slope1 == slope2):
-			d_1 = float("inf");
-		else :
-			x_int = ((y_mid_I - line[J,1])-slope1*x_mid_I +slope2*line[J,0])/(slope2-slope1);
-			y_int = y_mid_I +slope1*(x_int-x_mid_I);
-			d_1 = math.sqrt((x_mid_I-x_int)**2 + (y_mid_I-y_int)**2); 	
-	
-	##############################################################
-	x_mid_J =  (line[J,0]+line[J,2])/2;
-	y_mid_J =  (line[J,1]+line[J,3])/2;
-	if ((line[J,3] == line[J,1]) & (line[I,2] == line[I,0])):
-		d_2 = float("inf");
-	elif((line[J,3] == line[J,1])):
-		slope2 =(line[I,3]-line[I,1])/(line[I,2]-line[I,0]);
-		x_int = x_mid_J;
-		y_int = line[I,1] + slope2*(x_int-line[I,0]);
-		d_2 = math.sqrt((x_mid_J-x_int)**2 + (y_mid_J-y_int)**2);
-	elif ((line[I,2] == line[I,0])):
-		slope1 =(line[J,0]-line[J,2])/(line[J,3]-line[J,1]);
-		x_int = line[I,0];
-		y_int = y_mid_J +slope1*(x_int-x_mid_J);
-		d_2 = math.sqrt((x_mid_J-x_int)**2 + (y_mid_J-y_int)**2);
-	else :
-		slope2 =(line[I,3]-line[I,1])/(line[I,2]-line[I,0]);
-		slope1 = (line[J,0]-line[J,2])/(line[J,3]-line[J,1]);
-		if (slope1 == slope2):
-			d_2 = float("inf");
-		else :
-			x_int = ((y_mid_J - line[I,1])-slope1*x_mid_J +slope2*line[I,0])/(slope2-slope1);
-			y_int = y_mid_I +slope1*(x_int-x_mid_J);
-			d_2 = math.sqrt((x_mid_I-x_int)**2 + (y_mid_I-y_int)**2); 
-		
-	if ((d_1 ==float("inf")) | (d_2 ==float("inf"))):
-		length1 = math.sqrt((line[I,0]-line[I,2])**2 + (line[I,1]-line[I,3])**2);
-		length2 = math.sqrt((line[J,0]-line[J,2])**2 + (line[J,1]-line[J,3])**2);
-		data = np.asarray([I,J,intersection_valid[i][2],intersection_valid[i][3],d_1,d_2,length1,length2]);
-		vote_data = data;
-		vote_data_invalid.append(data);
-	else : 
-		if ((d_max  < d_1) | (d_max < d_2) ):
-			d_max = max(d_1,d_2);
-		length1 = math.sqrt((line[I,0]-line[I,2])**2 + (line[I,1]-line[I,3])**2);
-		length2 = math.sqrt((line[J,0]-line[J,2])**2 + (line[J,1]-line[J,3])**2);
-		#print d_1,d_2
-		data = np.asarray([I,J,intersection_valid[i][2],intersection_valid[i][3],d_1,d_2,length1,length2]);
-		vote_data = data;
-		vote_data_valid.append(data);
+for i in range(len(intersection_valid)):
+	if (intersection_valid[i][2]!=float("inf")):
+		cv2.circle(img,(int(intersection_valid[i][2]),int(intersection_valid[i][2])),3,(100,50,50),-1)
+		#print (intersection_valid[i][2],int(intersection_valid[i][2])
 
-	#(y-y_mid)/(x-x_mid) = slope1 ; 
-	#y = y_mid +slope1*(x-x_mid)   (i)
-	#(y-line[J,1])/(x-line[J,0]) = slope2 ; 
-	# y = line[j,1] + slope2*(x-line[j,0])   (ii)
-	#y_mid - line[j,1] = (slope2-slope1)*x +slope1*x_mid -slope2*line[j,0]
-	#((y_mid - line[j,1])-slope1*x_mid +slope2*line[j,0])/(slope2-slope1)=x
-	#x_int = ((y_mid_J - line[I,1])-slope1*x_mid_J +slope2*line[I,0])/(slope2-slope1);
-	#y_int = y_mid_J +slope1*(x_int-x_mid);
-	#d_2 = math.sqrt((x_mid_J-x_int)**2 + (y_mid_J-y_int)**2);[]
+
+cv2.imwrite('intersection_val_'+image_dir[-6:],img)
+img = img_hough
+'''
 
 
 
-print d_max;
-M =  np.shape(vote_data_valid)
-#print max(vote_data_invalid[:][5])
-vote = np.zeros((M[0],9));
-print M[0]
 
-for i in range(M[0]):
-	cost = w_1*(1-(vote_data_valid[i][4]/d_max))+w_1*(1-(vote_data_valid[i][5]/d_max)) + w_2*(vote_data_valid[i][6]/maxlength)+w_2*(vote_data_valid[i][7]/maxlength);
-	data = np.asarray([vote_data_valid[i][0],vote_data_valid[i][1],vote_data_valid[i][2],vote_data_valid[i][3],vote_data_valid[i][4],vote_data_valid[i][5],vote_data_valid[i][6],vote_data_valid[i][7],cost]);
-	vote[i,:] = data;
-perp_data_valid = np.zeros((M[0]*(M[0]-1)*(M[0]-2),16));
-#print (M[0]*(M[0]-1)*(M[0]-2)
-count = 0 ;
-for i in range(M[0]):
-	for j in range(i+1,M[0]):
-		for k in range(j+1,M[0]):
-			print count 
-			#print vote[i,2]
-			count = count + 1 ; 
-			diff1 = (float(vote[i,2])*float(vote[j,2]) - float(vote[i,3])*float(vote[j,3]))**2; 
-			diff2 = (float(vote[j,2])*float(vote[k,2]) - float(vote[j,3])*float(vote[k,3]))**2;
-			data = np.asarray([i,j,k,vote[i,2],vote[i,3],vote[j,2],vote[j,3],vote[k,2],vote[k,3],diff1,diff2,diff1+diff2,vote[i,8],vote[j,8],vote[k,8],vote[i,8]+vote[j,8]+vote[k,8]])
-			perp_data_valid[count,:] = data;
 
-n= np.argmax(vote[:,8]);
-m = np.argsort(perp_data_valid[:,11])[0];
-img = cv2.imread('groundtruth/Images/0000000041.jpg')
-print vote[n,0],vote[n,1]
-x1 = int(line[vote[n,0],0]);
-y1 = int(line[vote[n,0],1]);
-x2 = int(line[vote[n,0],2]);
-y2 = int(line[vote[n,0],3]);
-cv2.line(img,(x1,y1),(x2,y2),(0,255,0),2)
-x1 = int(line[vote[n,1],0]);
-y1 = int(line[vote[n,1],1]);
-x2 = int(line[vote[n,1],2]);
-y2 = int(line[vote[n,1],3]);
-cv2.line(img,(x1,y1),(x2,y2),(0,255,0),2)
-cv2.imwrite('houghlines_new.jpg',img)
-################################################
-img = cv2.imread('groundtruth/Images/0000000041.jpg')
-x1 = int(line[vote[perp_data_valid[m,0],0],0]);
-y1 = int(line[vote[perp_data_valid[m,0],0],1]);
-x2 = int(line[vote[perp_data_valid[m,0],0],2]);
-y2 = int(line[vote[perp_data_valid[m,0],0],3]);
-cv2.line(img,(x1,y1),(x2,y2),(0,255,0),2)
-x1 = int(line[vote[perp_data_valid[m,0],1],0]);
-y1 = int(line[vote[perp_data_valid[m,0],1],1]);
-x2 = int(line[vote[perp_data_valid[m,0],1],2]);
-y2 = int(line[vote[perp_data_valid[m,0],1],3]);
-cv2.line(img,(x1,y1),(x2,y2),(0,255,0),2)
-cv2.imwrite('houghlines_new_perp1.jpg',img)
-################################################
-img = cv2.imread('groundtruth/Images/0000000041.jpg')
-x1 = int(line[vote[perp_data_valid[m,1],0],0]);
-y1 = int(line[vote[perp_data_valid[m,1],0],1]);
-x2 = int(line[vote[perp_data_valid[m,1],0],2]);
-y2 = int(line[vote[perp_data_valid[m,1],0],3]);
-cv2.line(img,(x1,y1),(x2,y2),(0,255,0),2)
-x1 = int(line[vote[perp_data_valid[m,1],1],0]);
-y1 = int(line[vote[perp_data_valid[m,1],1],1]);
-x2 = int(line[vote[perp_data_valid[m,1],1],2]);
-y2 = int(line[vote[perp_data_valid[m,1],1],3]);
-cv2.line(img,(x1,y1),(x2,y2),(0,255,0),2)
-cv2.imwrite('houghlines_new_perp2.jpg',img)
-################################################
-img = cv2.imread('groundtruth/Images/0000000041.jpg')
-x1 = int(line[vote[perp_data_valid[m,2],0],0]);
-y1 = int(line[vote[perp_data_valid[m,2],0],1]);
-x2 = int(line[vote[perp_data_valid[m,2],0],2]);
-y2 = int(line[vote[perp_data_valid[m,2],0],3]);
-cv2.line(img,(x1,y1),(x2,y2),(0,255,0),2)
-x1 = int(line[vote[perp_data_valid[m,2],1],0]);
-y1 = int(line[vote[perp_data_valid[m,2],1],1]);
-x2 = int(line[vote[perp_data_valid[m,2],1],2]);
-y2 = int(line[vote[perp_data_valid[m,2],1],3]);
-cv2.line(img,(x1,y1),(x2,y2),(0,255,0),2)
-cv2.imwrite('houghlines_new_perp3.jpg',img)
-################################################
-img = cv2.imread('groundtruth/Images/0000000041.jpg')
-x1 = int(line[vote[perp_data_valid[m,0],0],0]);
-y1 = int(line[vote[perp_data_valid[m,0],0],1]);
-x2 = int(line[vote[perp_data_valid[m,0],0],2]);
-y2 = int(line[vote[perp_data_valid[m,0],0],3]);
-cv2.line(img,(x1,y1),(x2,y2),(0,255,0),2)
-x1 = int(line[vote[perp_data_valid[m,0],1],0]);
-y1 = int(line[vote[perp_data_valid[m,0],1],1]);
-x2 = int(line[vote[perp_data_valid[m,0],1],2]);
-y2 = int(line[vote[perp_data_valid[m,0],1],3]);
-cv2.line(img,(x1,y1),(x2,y2),(0,255,0),2)
-x1 = int(line[vote[perp_data_valid[m,1],0],0]);
-y1 = int(line[vote[perp_data_valid[m,1],0],1]);
-x2 = int(line[vote[perp_data_valid[m,1],0],2]);
-y2 = int(line[vote[perp_data_valid[m,1],0],3]);
-cv2.line(img,(x1,y1),(x2,y2),(0,255,0),2)
-x1 = int(line[vote[perp_data_valid[m,1],1],0]);
-y1 = int(line[vote[perp_data_valid[m,1],1],1]);
-x2 = int(line[vote[perp_data_valid[m,1],1],2]);
-y2 = int(line[vote[perp_data_valid[m,1],1],3]);
-cv2.line(img,(x1,y1),(x2,y2),(0,255,0),2)
-x1 = int(line[vote[perp_data_valid[m,2],0],0]);
-y1 = int(line[vote[perp_data_valid[m,2],0],1]);
-x2 = int(line[vote[perp_data_valid[m,2],0],2]);
-y2 = int(line[vote[perp_data_valid[m,2],0],3]);
-cv2.line(img,(x1,y1),(x2,y2),(0,255,0),2)
-x1 = int(line[vote[perp_data_valid[m,2],1],0]);
-y1 = int(line[vote[perp_data_valid[m,2],1],1]);
-x2 = int(line[vote[perp_data_valid[m,2],1],2]);
-y2 = int(line[vote[perp_data_valid[m,2],1],3]);
-cv2.line(img,(x1,y1),(x2,y2),(0,255,0),2)
-cv2.imwrite('houghlines_new_perp.jpg',img)'''
+
+
+
+
+
+
+
