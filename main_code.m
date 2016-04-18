2b15a8df43d297140c56cac2a7fe87970e985a6c
nithin127 <nithin127vasisth@gmail.com>
2016-04-18 09:06:08 +0530

    detected intersection points

--- main_code.m
+++ main_code.m
@@ -4,17 +4,30 @@
 % the spacial layout of Cluttered Rooms 
 %"http://vision.cs.uiuc.edu/~vhedau2/Research/research_spatialLayout.html"
 
+clear;
+close all
 %image = imread('image.jpg');
 image = imread('groundtruth/Images/2884291786_69bec3d738_m.jpg');
 %image = imread('groundtruth/Images/0000000041.jpg');
 grayIm = rgb2gray(image);
 minLen = 0.025*sqrt(size(image,1)*size(image,2));
-lines = APPgetLargeConnectedEdges(grayIm, minLen);
 
+lines = APPgetLargeConnectedEdges(grayIm, minLen);
+%{
 % displaying image
 figure(1), hold off, imshow(grayIm)
 figure(1), hold on, plot(lines(:, [1 2])', lines(:, [3 4])')
+%}
 
 % let's try to detect the vanishing points using this information
 
+cross = find_intersection(lines);
 
+%{
+%plot intersection points
+ind = find((cross(:,2) ~= inf)&(cross(:,5)==1));
+hold on
+plot(cross(ind,1),cross(ind,2),'o')
+%}
+
+rank
