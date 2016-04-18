481ea1506775bb1d0da6f90af2d5a9c1667da563
nithin127 <nithin127vasisth@gmail.com>
2016-04-18 07:07:14 +0530

    New approach using matlab

---
+++ main_code.m
@@ -0,0 +1,20 @@
+% This is the main code for the computer project [CS-763], IIT Bombay.
+% Developers: Nithin Vasisth, Pulkit Katdare
+% This code mainly follows the paper written by Varsha Hedao: Recovering
+% the spacial layout of Cluttered Rooms 
+%"http://vision.cs.uiuc.edu/~vhedau2/Research/research_spatialLayout.html"
+
+%image = imread('image.jpg');
+image = imread('groundtruth/Images/2884291786_69bec3d738_m.jpg');
+%image = imread('groundtruth/Images/0000000041.jpg');
+grayIm = rgb2gray(image);
+minLen = 0.025*sqrt(size(image,1)*size(image,2));
+lines = APPgetLargeConnectedEdges(grayIm, minLen);
+
+% displaying image
+figure(1), hold off, imshow(grayIm)
+figure(1), hold on, plot(lines(:, [1 2])', lines(:, [3 4])')
+
+% let's try to detect the vanishing points using this information
+
+
